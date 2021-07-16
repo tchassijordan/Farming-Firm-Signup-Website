2fe103e42d7bad236b1af275a600b60e6d4b7aac
tchassi jordan <tchassijordan@icloud.com>
2021-07-16 14:57:17 +0100

    farming site signup respo

---
+++ README.md
@@ -0,0 +1,42 @@
+# Atenna Signup
+Atenna Signup is the realization of a sign up and sign in to a company called ATENNA. 
+
+The user creates an account on ATENNA's main site.  
+
+Certain verifications are done before the signup can be valid like: 
+<li>If the account doesn't exist already</li>
+<li>If the data enter follow the norms and are valid</li>  
+
+After signup complete the user is redirected to a a user account page. 
+
+The user also has the possibility to sign in to an already existing account via login, verifications are also made to ensure account is valaid and in the system already. After the verifications the user is redirected to user account page. 
+
+I used the browser's local storage for all data persistency.
+
+---
+
+## Preview
+
+<img src="./images/signup.png" alt="signup">
+
+---
+
+## Coming Soon
+- [ ] External storage implementation
+- [ ] Multiple accounts
+
+---
+
+## Contributors
+ <a href="https://github.com/tchassijordan">
+ tchassi jordan</a>
+
+---
+
+## Contributing 
+Pull requests are welcome. For major changes/propose impovements, please open an issue first to discuss what you would like to change.
+
+---
+
+<h3 align="left">Languages and tools:</h3>
+<p align="left"> <a href="https://www.w3schools.com/css/" target="_blank"> <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/css3/css3-original-wordmark.svg" alt="css3" width="40" height="40"/> </a> <a href="https://www.w3.org/html/" target="_blank"> <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/html5/html5-original-wordmark.svg" alt="html5" width="40" height="40"/> </a> <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank"> <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/javascript/javascript-original.svg" alt="javascript" width="40" height="40"/> </a> </p>
---
+++ javascript/signup.js
@@ -0,0 +1,131 @@
+var form1 = document.getElementById("form1"); //Email and username form section
+var form2 = document.getElementById("form2"); //Form section for password
+var next = document.getElementById("next"); //next button
+var back = document.getElementById("back"); //back button
+var email = document.getElementById("email"); //email field
+var username = document.getElementById("name"); //username field
+var login = document.getElementById("login"); //initial login button
+var signup = document.getElementById("signup"); //signup button
+var login_section = document.getElementById("login-container"); //login form
+var regEx = /\S+@\S+\.\S+/;
+var input1 = form1.getElementsByTagName("input"); //An array of all input fields in sign up email section
+var input2 = form2.getElementsByTagName("input"); //An array of all input fields in sign up password section
+var input3 = login_section.getElementsByTagName("input");  //An array of all input fields in Log in section
+var submit_login = document.getElementById("submit-login"); //second login button
+var submit_signup = document.getElementById("submit-signup"); //button to complete sign up
+var object = {
+    "Email": "",
+    "Username": "",
+    "Password": ""
+}; //object for the storage of form data
+
+signup.addEventListener("click", ()=> {
+    form1.style.display = "block";
+    signup.style.display = "none";
+    login.style.display = "none";
+}); //this dispalys form1 containing the email section when the initial sign up button is clicked
+
+login.addEventListener("click", ()=> {
+    login_section.style.display = "block";
+    signup.style.display = "none";
+    login.style.display = "none";
+}); //this displays the login in form when the initail log in button is clicked  
+
+submit_signup.addEventListener('click', store);
+
+function store() {
+    if (input2[1].value !== input2[0].value) {
+        input2[1].style.backgroundColor = "red";
+        document.getElementById("mismatch").style.display = "block";
+        /* When the value of password and confirm password aren't thesame change confirm 
+        password background color red */
+    } 
+    else if (input2[1].value == input2[0].value) {
+        if (input2[0].value !== entryPassword) {
+            input2[1].style.backgroundColor = "white";
+            document.getElementById("mismatch").style.display = "none";
+            object.Email = input1[0].value; 
+            object.Username = username.value;
+            object.Password = input2[0].value; 
+            strg = JSON.stringify(object);
+            back_strg = JSON.parse(strg);
+            localStorage.setItem("Data", strg);
+            console.log(back_strg); //the object isn't being displayed in console because of window.location 
+            /* Only if the passwords match, the values of the different form input are 
+            stored in an object */
+            window.location = "user-account.html"; //when all is done redirect to userAccount
+        } else {
+            alert("input another password");
+        } 
+    }  
+} 
+
+input2[1].addEventListener('keyup', ()=> {
+    if (input2[1].value !== input2[0].value) {
+        input2[1].style.backgroundColor = "red";
+        document.getElementById("mismatch").style.display = "block";
+    } else if (input2[1].value == input2[0].value) {
+        input2[1].style.backgroundColor = "white";
+        document.getElementById("mismatch").style.display = "none";
+    }
+}); //test if the value entered in password and confirm fields are thesame 
+
+next.addEventListener("click",check);//runs check function when next button is clicked
+
+function check() {
+    if(input1[0].value !=="" && input1[1].value !=="") {
+        entryData();
+        if(input1[0].value !== entryEmail && input1[1].value !== entryUserName) {
+            form2.style.display = "block";
+            form1.style.display = "none";  //need to check email and username 
+             /* At the end of the first if, proceeds to displaying form2 containing password when both values of
+             the input email and username aren't emtpy or in the system already */ 
+        } else if (input1[0].value == entryEmail && input1[1].value == entryUserName) {
+            alert("Email and Username already in the system");
+        } else if(input1[0].value == entryEmail) {
+            alert("Email already in the system");
+        } else if (input1[1].value == entryUserName) {
+            alert("Username already in the system");
+        } 
+    } else {alert("please fill the input fields to continue");}
+   //the first if checks if the input values aren't blank     
+} 
+
+back.addEventListener("click", ()=> {
+    form2.style.display = "none";
+    form1.style.display = "block";
+});//displays form1 when clicked on i.e goes back
+
+submit_login.addEventListener("click", ()=> {
+    userAccount();
+}); 
+
+function initialize() {
+    strg = JSON.stringify(object);
+    localStorage.setItem("Data", strg);
+    entryJSON = localStorage.getItem("Data"); //retrieving account data from storage
+} //used to initialize local storage null
+
+function entryData() {
+    entryJSON = localStorage.getItem("Data"); //retrieving account data from storage
+    if(!entryJSON) {
+        initialize();
+    }
+    entry = JSON.parse(entryJSON); //Parsing the account data stored locally back as an object for test
+    entryEmail = entry.Email;
+    entryPassword = entry.Password;
+    entryUserName = entry.Username;
+}
+
+function userAccount(event) {
+    entryData();
+    if (input3[0].value !== "" && input3[1].value !== "") {
+        if((input3[0].value == entryEmail) && (input3[1].value == entryPassword)) {
+                console.log("valid");
+                window.location = "user-account.html"; //when all is done redirect to userAccount
+        } else {
+            alert("account not found");
+        }
+    } else {alert("fill the inputs");}
+    
+} //need to add a loop to address multiple values
